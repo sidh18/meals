231b0cb820b0cb6bfaaf11b4d5306cf94b0e8249
sidh <kochetasidh18@gmail.com>
2023-10-02 19:42:21 +0530

    adding metadata with price and complexity

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal,});
 
   final Meal meal;
+
+  String get complexityText{
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
+
+  String get affordabilityText{
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin:const EdgeInsets.all(8),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15),
+      margin: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
       ),
       clipBehavior: Clip.hardEdge,
       elevation: 2,
@@ -20,14 +31,12 @@ class MealItem extends StatelessWidget {
         child: Stack(
           children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl),
-                fit:BoxFit.cover,
-                height: 200,
-                width:double.infinity,
-
-                ),
-                
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
             Positioned(
               bottom: 0,
               left: 0,
@@ -38,21 +47,30 @@ class MealItem extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
                 child: Column(
                   children: [
-                    Text(
-                      meal.title,
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                      style:const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color:Colors.white,
-                      )
-                    ),
+                    Text(meal.title,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
                     const SizedBox(height: 12),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule, label: '${meal.duration}min'),
+                            const SizedBox(width:12),
+                             MealItemTrait(
+                            icon: Icons.work, label:complexityText),
+                            const SizedBox(width:12),
+                             MealItemTrait(
+                            icon: Icons.attach_money, label: affordabilityText),
+                            
+                      ],
                     )
                   ],
                 ),
---
+++ lib/widgets/meal_item_trait.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MealItemTrait extends StatelessWidget{
+  const MealItemTrait({super.key,required this.icon,required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Icon(icon,size: 19, color:const Color.fromARGB(255, 243, 242, 238)),
+      const SizedBox(width:6),
+      Text(label,style: const TextStyle(color: Colors.white),),
+    ],);
+  }
+   
+}
