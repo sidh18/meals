0f313e75adbc918863641da67f3fa853271249c2
sidh <kochetasidh18@gmail.com>
2023-10-13 00:23:45 +0530

    hiding debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,9 @@ final theme=ThemeData(
 
 void main(){
   runApp(const App());
+  
+
+
 }
 
 class App extends StatelessWidget{
@@ -25,6 +28,7 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: theme,
       home:const TabsScreen(),
     );
